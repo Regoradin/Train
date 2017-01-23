4f31cd93b423a0faf335d61ae2e3e9a605758bf0
Regoradin <oliver@theabates.com>
2017-01-22 22:17:28 -0600

    First Commit + Animations
    Unity exists, along with a voxel conductor model who has basic movement
    animations and a player mover script which should be good. More
    animations have been imported, but they still have to be added to the
    conductor.

---
+++ Train/Assets/Scripts/Animation Controllers/PlayerAnimationController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimationController : MonoBehaviour {
+
+	private Animator animator;
+
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	void Update () {
+
+		//walking and running animations
+		animator.SetFloat("speed_vertical", Input.GetAxis("Vertical"));
+		if (Input.GetAxis("Vertical") != 0)
+		{
+			animator.SetFloat("running", Input.GetAxis("Run"));
+		}
+		else
+		{
+			animator.SetFloat("running", 0f);
+		}
+
+		//turning animation and motion
+		if (Input.GetAxis("Horizontal") > 0)
+		{
+			if (Input.GetAxis("Vertical") == 0f)
+			{
+				animator.SetBool("turning_right", true);
+			}
+			transform.Rotate(Vector3.up, 96.4f * Time.deltaTime);
+		}
+		else
+		{
+			animator.SetBool("turning_right", false);
+		}
+		if(Input.GetAxis("Horizontal") < 0)
+		{
+			if (Input.GetAxis("Vertical") == 0f)
+			{
+				animator.SetBool("turning_left", true);
+			}
+			transform.Rotate(Vector3.up, -96.4f * Time.deltaTime);
+		}
+		else
+		{
+			animator.SetBool("turning_left", false);
+		}
+
+	}
+}
---
+++ Train/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float move_speed;
+	public float look_speed;
+
+	new private Camera camera;
+	private Rigidbody rb;
+
+	void Start () {
+
+		camera = GetComponentInChildren<Camera>();
+		rb = GetComponent<Rigidbody>();
+
+	}
+	
+	void Update () {
+
+		//takes player input for moving around
+		Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+		movement *= move_speed;
+
+		//moves the player relative to their orientation, not the world
+		movement = transform.TransformVector(movement);
+
+		rb.velocity = movement;
+
+		//takes player input for looking around
+		Vector3 look = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0f);
+		look *= look_speed;
+
+		//rotates the body for the y-axis rotation and the "head" camera for x-axis
+		rb.angularVelocity = look;
+		camera.transform.Rotate(Vector3.right, look.x);
+
+	}
+}
