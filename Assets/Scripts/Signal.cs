35a7ed5c7ffce4d468e6a008331c2a9e6691b2ed
Regoradin <oliver@theabates.com>
2017-02-26 11:16:41 -0600

    Signalling Attempt
    An attempt to add signaling and stuff, using events. I don't know how
    that works though,  so it is still veeeeery WIP.

---
+++ Assets/Scripts/Signal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signal : MonoBehaviour {
+	//this script creates a one directional signal block between two track objects, and modifies the gameobject it is sitting on to indicate status
+
+	public GameObject opening_track;
+	public GameObject closing_track;
+
+	private List<GameObject> carriages_in_block;
+
+	void Start () {
+		carriages_in_block = new List<GameObject>();
+	}
+	
+}
