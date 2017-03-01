9938fbaf402bb9b43e5fdcb094f31dd6a20c9fa1
Regoradin <oliver@theabates.com>
2017-03-01 12:01:41 -0600

    Signal v1
    Signals are now possible with 1-in 1-out track pieces defined by two
    track components.

--- Assets/Scripts/Signal.cs
+++ Assets/Scripts/Signal.cs
@@ -8,10 +8,43 @@ public class Signal : MonoBehaviour {
 	public GameObject opening_track;
 	public GameObject closing_track;
 
+	public Color empty_block_color;
+	public Color filled_block_color;
+
 	private List<GameObject> carriages_in_block;
 
 	void Start () {
 		carriages_in_block = new List<GameObject>();
+
+		//add removing and adding carriages to the approriate events
+		opening_track.GetComponent<Track>().OnCollision += AddCarriage;
+		closing_track.GetComponent<Track>().OnCollision += RemoveCarriage;
+		UpdateState();
 	}
-	
+
+	void AddCarriage(GameObject carriage)
+	{
+		carriages_in_block.Add(carriage);
+		UpdateState();
+	}
+
+	void RemoveCarriage(GameObject carriage)
+	{
+		carriages_in_block.Remove(carriage);
+		UpdateState();
+	}
+
+	void UpdateState()
+	{
+		Renderer rend = GetComponent<Renderer>();
+
+		if(carriages_in_block.Count == 0)
+		{
+			rend.material.color = empty_block_color;
+		}
+		else
+		{
+			rend.material.color = filled_block_color;
+		}
+	}
 }
--- Assets/Scripts/Track.cs
+++ Assets/Scripts/Track.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class Track : MonoBehaviour {
 
-	 
+	public delegate void CollisionAction(GameObject gameObject);
+	public event CollisionAction OnCollision;
 
+	//whenever a carriage collides with this track, sends and event to whomever is listening telling them that there is a carriage passing through
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Carriage")
+		{
+			if (OnCollision != null)
+			{
+				OnCollision(other.gameObject);
+			}
+		}
+	}
 }
