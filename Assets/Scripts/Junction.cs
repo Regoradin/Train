aa5a871f4a3e28d8560028e019427807675af997
Regoradin <oliver@theabates.com>
2017-03-19 20:19:49 -0500

    More Junction Crap
    Still not 100% functional. To do: change branch specification to use
    parent objects to specify things, and not lists of individual stuff.

--- Assets/Scripts/Junction.cs
+++ Assets/Scripts/Junction.cs
@@ -15,13 +15,15 @@ public class Junction : MonoBehaviour {
 	private List<Collider> right_out_colliders;
 
 	private List<GameObject> junction_tracks;
+	private List<Collider> junction_colliders;
 
 	private List<GameObject> carriages_in_junction;
 
 	void Start()
 	{
-		//subcsribes all the things to the OnCollision event if they exist
 		carriages_in_junction = new List<GameObject>();
+		junction_tracks = new List<GameObject>();
+		junction_colliders = new List<Collider>();
 
 		//setting up colliders
 		in_colliders = SetUpColliders(in_tracks);
@@ -29,12 +31,19 @@ public class Junction : MonoBehaviour {
 		center_out_colliders = SetUpColliders(center_out_tracks);
 		right_out_colliders = SetUpColliders(right_out_tracks);
 
+
+
 		//assembling junction_tracks
 		junction_tracks.AddRange(in_tracks);
 		junction_tracks.AddRange(left_out_tracks);
 		junction_tracks.AddRange(center_out_tracks);
 		junction_tracks.AddRange(right_out_tracks);
 
+		//assembling junction_colliders
+		junction_colliders.AddRange(in_colliders);
+		junction_colliders.AddRange(left_out_colliders);
+		junction_colliders.AddRange(center_out_colliders);
+		junction_colliders.AddRange(right_out_colliders);
 
 		//setting up events, it should work to just have the first one be the one that is in the list, but maybe not. Hopefully remember to order lists with the farthest out track first
 		in_tracks[0].GetComponent<Track>().OnTrainCross += EnterJunction;
@@ -69,6 +78,7 @@ public class Junction : MonoBehaviour {
 		Debug.Log(carriage.name + " entering junction");
 		if(carriages_in_junction.Count == 0)
 		{
+			Debug.Log("Train entering jucniton");
 			//if the junction is currently empty, i.e. it is still in an accepting new trains states, switch the subscribed functions
 			if (in_tracks.Contains(track))
 			{
@@ -161,20 +171,39 @@ public class Junction : MonoBehaviour {
 
 	void LeaveJunction(GameObject track, GameObject carriage)
 	{
+		Debug.Log(carriage.name + " leaving junction");
 		carriages_in_junction.Remove(carriage);
 
 		//if every carriage has left the junction, set everything back to normal
 		if (carriages_in_junction.Count == 0)
 		{
-			foreach (GameObject junction_track in junction_tracks)
-			{
-				//sets everything back to normal
-				junction_track.GetComponent<Track>().OnTrainCross -= LeaveJunction;
-				junction_track.GetComponent<Track>().OnTrainCross -= EnterJunction;     //this is just to remove the enter junction from the one track that was being entered through, to prevent doubling up functions
-				junction_track.GetComponent<Track>().OnTrainCross += EnterJunction;
+			Debug.Log("Train leaving Junction");
 
-				junction_track.GetComponent<Collider>().enabled = true;
+			//sets everything back to normal
+			in_tracks[0].GetComponent<Track>().OnTrainCross -= LeaveJunction;
+			in_tracks[0].GetComponent<Track>().OnTrainCross -= EnterJunction;     //this is just to remove the enter junction from the one track that was being entered through, to prevent doubling up functions
+			in_tracks[0].GetComponent<Track>().OnTrainCross += EnterJunction;
+
+			left_out_tracks[0].GetComponent<Track>().OnTrainCross -= LeaveJunction;
+			left_out_tracks[0].GetComponent<Track>().OnTrainCross -= EnterJunction;
+			left_out_tracks[0].GetComponent<Track>().OnTrainCross += EnterJunction;
+
+			center_out_tracks[0].GetComponent<Track>().OnTrainCross -= LeaveJunction;
+			center_out_tracks[0].GetComponent<Track>().OnTrainCross -= EnterJunction;
+			center_out_tracks[0].GetComponent<Track>().OnTrainCross += EnterJunction;
+
+			right_out_tracks[0].GetComponent<Track>().OnTrainCross -= LeaveJunction;
+			right_out_tracks[0].GetComponent<Track>().OnTrainCross -= EnterJunction;
+			right_out_tracks[0].GetComponent<Track>().OnTrainCross += EnterJunction;
+
+
+			foreach (Collider collider in junction_colliders)
+			{
+				//Debug.Log(collider.name + " " + collider.enabled);
+				collider.enabled = true;
+				//Debug.Log(collider.name + " " + collider.enabled);
 			}
+
 		}
 	}
 	
