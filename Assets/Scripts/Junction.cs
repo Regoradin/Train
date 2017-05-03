d7f7ae7c0865fb161d78055e22d517265276b543
Regoradin <oliver@theabates.com>
2017-05-03 11:42:40 -0500

    Functional Junctions
    Junctions are now functional, with several issues:
    1. Alternate tracks dissapear instead of just becoming not drive-on-able
    2. Sometimes the last cart is still far enough in the junction to be
    misdirected when it triggers the last track.

--- Assets/Scripts/Junction.cs
+++ Assets/Scripts/Junction.cs
@@ -16,12 +16,14 @@ public class Junction : MonoBehaviour {
 
 	//dictionary with each path identified by string name and a list of the gameobjects comprising them
 	private Dictionary<string, List<GameObject>> paths;
+	private Dictionary<string, GameObject> entrances;
 
 	private void Start()
 	{
 		//initializing stuff
 		carriages_in_junction = new List<GameObject>();
 		paths = new Dictionary<string, List<GameObject>>();
+		entrances = new Dictionary<string, GameObject>();
 
 		//Sets up paths to each have their exit and the central entrance branch
 		paths["left"] = GetTracks(left_parent);
@@ -33,12 +35,18 @@ public class Junction : MonoBehaviour {
 			paths[key].AddRange(GetTracks(entrance_parent));
 		}
 
+		//Sets up entrances
+		entrances["entrance"] = entrance_parent;
+		entrances["left"] = left_parent;
+		entrances["center"] = center_parent;
+		entrances["right"] = right_parent;
 
 		//add events to listen for entraces to the junction
 		entrance_parent.GetComponent<Track>().OnTrainCross += Enter;
 		left_parent.GetComponent<Track>().OnTrainCross += ReverseEnter;
 		center_parent.GetComponent<Track>().OnTrainCross += ReverseEnter;
 		right_parent.GetComponent<Track>().OnTrainCross += ReverseEnter;
+
 
 	}
 
@@ -48,7 +56,10 @@ public class Junction : MonoBehaviour {
 		List<GameObject> tracks = new List<GameObject>();
 
 		//adds itself to the list
-		tracks.Add(parent);
+		if (parent.tag == "Track")
+		{
+			tracks.Add(parent);
+		}
 
 		//builds a list of all the children that are tracks
 		foreach(Transform child in parent.transform)
@@ -65,9 +76,9 @@ public class Junction : MonoBehaviour {
 
 	private void Enter(GameObject entered_track, GameObject carriage)
 	{
-		string direction = carriage.GetComponent<TrainController>().Direction;
+		string direction = carriage.GetComponentInParent<TrainController>().Direction;
 
-		//turning on only appropriate track
+		//turning off all tracks, and then only turn on appropriate track
 		foreach (string key in paths.Keys)
 		{
 			foreach(GameObject track in paths[key])
@@ -80,10 +91,66 @@ public class Junction : MonoBehaviour {
 		{
 			track.SetActive(true);
 		}
+
+		//adjusting event listening on the chosen path 
+		//removing the waiting for entrance events
+		entrances["entrance"].GetComponent<Track>().OnTrainCross -= Enter;
+		entrances[direction].GetComponent<Track>().OnTrainCross -= ReverseEnter;
+		//adding events for the train being in the junction
+		entrances["entrance"].GetComponent<Track>().OnTrainCross += OpenEnter;
+		entrances[direction].GetComponent<Track>().OnTrainCross += OpenExit;
+
 	}
 
 	private void ReverseEnter(GameObject entered_track, GameObject carriage)
 	{
+
+	}
+
+	//OpenEnter and OpenExit are functions that listen to carriages entering and leaving a junction that has already been opened. As such, entered_track should be the track that is the entrance or exit, respectively
+	private void OpenEnter(GameObject entered_track, GameObject carriage)
+	{
+		carriages_in_junction.Add(carriage);
+	}
+
+	private void OpenExit(GameObject entered_track, GameObject carriage)
+	{
+		carriages_in_junction.Remove(carriage);
+
+		if(carriages_in_junction.Count == 0)  //When the junction is empty, call Reset on all active entrances
+		{
+			foreach(KeyValuePair<string, GameObject> entrance in entrances)
+			{
+				if(entrance.Value.activeSelf == true)
+				{
+					Reset(entrance.Value);
+				}
+			}
+		}
+	}
+
+	private void Reset(GameObject entered_track)
+	{
+
+		//reset event listening
+		entered_track.GetComponent<Track>().OnTrainCross -= OpenEnter;
+		if (entered_track = entrances["entrance"])
+		{
+			entered_track.GetComponent<Track>().OnTrainCross += Enter;
+		}
+		else
+		{
+			entered_track.GetComponent<Track>().OnTrainCross += ReverseEnter;
+		}
+
+		//turn deactivated paths back on
+		foreach(string key in paths.Keys)
+		{
+			foreach (GameObject track in paths[key])
+			{
+				track.SetActive(true);
+			}
+		}
 
 	}
 }
