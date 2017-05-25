a898e02db2aba0bd9660af8ff2133a8f9930f383
Regoradin <oliver@theabates.com>
2017-05-25 13:57:22 -0500

    Start of Turrets Turrets and mostly functional now, and kinda sorta shoot bullets in a way.

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+	public Seat seat;
+	public GameObject bullet;
+
+	public float fire_speed;
+	public float muzzle_velocity;
+
+	void Update()
+	{
+		if (seat.Seated)
+		{
+			if (Input.GetKey("mouse 0"))
+			{
+				Shoot(bullet);
+			}
+		}
+	}
+
+	void Shoot(GameObject bullet)
+	{
+		GameObject new_bullet = Instantiate(bullet, transform.position + Vector3.forward * 20, transform.rotation);
+
+		new_bullet.GetComponent<Rigidbody>().velocity = muzzle_velocity * Vector3.forward;
+
+	}
+
+}
