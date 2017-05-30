91623c03f16d047e8849754df8e03a673c205cda
Regoradin <oliver@theabates.com>
2017-05-29 20:25:14 -0500

    Station Shop Basics Basic idea of what station buying and selling will work like. Buttons are annoying.

--- Assets/Scripts/Train Interaction Components/Turrets/Bullet.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Bullet : MonoBehaviour {
-
-	//this would be where all the fun effects and stuff for various bullets would be stored (burning, graphical effects, homing stuff, etc.)
-}
