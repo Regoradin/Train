dee8e7a9b9ac2cae30c5c23cb9968c30034c3712
Regoradin <oliver@theabates.com>
2017-06-01 09:35:56 -0500

    Player UI
    Added a player UI and super basic player inventory system to run it.
    Currently displays a centered health bar and a money counter on screen
    always.

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour {
+
+	public float max_health;
+	private float health;
+	public float Health
+	{
+		get
+		{
+			return health;
+		}
+		set
+		{
+			if (value > max_health)
+			{
+				health = max_health;
+			}
+			if(value < 0)
+			{
+				health = 0;
+			}
+			else
+			{
+				health = value;
+			}
+		}
+	}
+
+	private float money = 0;
+	public float Money
+	{
+		get
+		{
+			return money;
+		}
+		set
+		{
+			if (value >= 0)
+			{
+				money = value;
+			}
+			else
+			{
+				money = 0;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour {
+
+	private Canvas canvas;
+
+	public Text money;
+	public Image health;
+
+	private PlayerInventory inventory;
+
+	public float health_number; //these will need to be grabbed from an outside script once it exists
+	public float max_health;
+	public float health_offset;
+
+	void Start()
+	{
+		canvas = GetComponentInParent<Canvas>();
+
+		inventory = GetComponentInParent<PlayerInventory>();
+	}
+
+	private void Update()
+	{
+		money.text = "$" + inventory.Money;
+
+		float health_percent = health_number / max_health;
+
+		health.rectTransform.sizeDelta = new Vector2((Screen.width - health_offset) * health_percent, health.rectTransform.rect.height);
+
+	}
+
+
+}
